eda75d200716be67d4c230b9acc6e1306a0778d9
Suhaib <enigma00800@gmail.com>
2022-12-01 01:22:27 +0530

    v-1.10

--- script.js
+++ script.js
@@ -3,44 +3,51 @@ let band2 = document.getElementById("band2");
 let band3 = document.getElementById("band3");
 let band4 = document.getElementById("band4");
 let band5 = document.getElementById("band5");
-let i = 0;
+let container = document.getElementById("colorPallete");
+let result = document.getElementById("result")
 let resistance = "";
 const bands = [band1, band2, band3, band4, band5];
 const colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white", "goldenrod", "silver"];
 const tolerance = ["20", "1", "2", "3", "  +100", "0.5", "0.25", "0.10", "0.05", "10", "5", "10"];
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button) => {
-    button.addEventListener('click', (e) => {
-        let power = e.target.value;
 
-        if (i < 3) {
-            if (e.target.value > 9) {
-                power = "";
-            }
-            resistance = resistance + power;
-            document.getElementById('R').innerHTML = resistance;
+function createColorpallete() {
+    let input;
+    for (let i = 0; i < colors.length; i++) {
+        input = document.createElement("input");
+        input.value = i;
+        input.classList.add("button");
+        input.style.backgroundColor = `${colors[i]}`;
+        container.appendChild(input);
+        input.disabled = true;
+    }
+}
+createColorpallete();
+// ----------------------
+let cb = 0;
+container.addEventListener('click', myFunction);
+function myFunction(e) {
+    if (e.target !== e.currentTarget)
+        console.log(e.target.value);
+    let selectedColor = e.target.value;
+    if (cb < 3) {
+        if (selectedColor > 9) { alert("Please select a valid color"); }
+        else {
+            bands[cb++].style.backgroundColor = colors[selectedColor];
+            resistance = resistance + selectedColor;
         }
-        else if (i == 3) {
-            if (e.target.value > 9) {
-                power = 9 - power;
-            }
-            resistance = resistance * Math.pow(10, power);
-            document.getElementById('R').innerHTML = resistance;
+    }
+    else if (cb == 3) {
 
-        }
-        if (i == 4) {
+        bands[cb++].style.backgroundColor = colors[selectedColor];
+        resistance = resistance + " x 10" + selectedColor.sup() + " ";
 
-            let sign = " ± ";
-            if (power == 4) sign = "";
-            resistance = resistance + sign + tolerance[power] + "%";
-            document.getElementById('R').innerHTML = resistance;
+    }
+    else if (cb == 4) {
+        let sign = " ± ";
+        if (selectedColor == 4) sign = "";
+        bands[cb++].style.backgroundColor = colors[selectedColor];
+        resistance = resistance + sign + tolerance[selectedColor] + "%";
+    }
+    result.innerHTML = resistance;
 
-        }
-        if (power === '') {
-            bands[i].style.border = "none";
-        }
-
-        bands[i].style.backgroundColor = colors[e.target.value];
-        i++;
-    })
-})
+}
